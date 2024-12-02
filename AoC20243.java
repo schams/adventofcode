58009540723373098e4f2c644738fff1b9f5e90c
dschams <dan.schams@gmail.com>
2024-12-01 23:23:23 -0600

    day 2 problem 1 failed attempt

---
+++ AoC20243.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.lang.Math;
+import java.lang.String;
+import java.util.HashSet;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public class AoC20243 {
+    public static void main(String[] args) {
+        int safeReports = 0;
+        try {
+            String input = Files.readString(Path.of("input3.txt"));
+            for( String line : input.split("\n") ) {
+                ArrayList<Integer> levels = new ArrayList<>(Arrays.stream(line.split("\\s+")).map( inputLevel -> Integer.parseInt(inputLevel)).collect(Collectors.toList()));
+                boolean isDescending = levels.get(0) > levels.get(1);
+                boolean isSafe = true;
+                if( isDescending ) {
+                    for( int i = 2; i < levels.size(); i++ ) {
+                        int change = levels.get(i-1) - levels.get(i);
+                        if(  change < 1 || change > 3 ) {
+                            isSafe = false;
+                            break;
+                        }
+                    }
+                } else {
+                    for( int i = 2; i < levels.size(); i++ ) {
+                        int change = levels.get(i) - levels.get(i-1);
+                        if(  change < 1 || change > 3 ) {
+                            isSafe = false;
+                            break;
+                        }
+                    }
+                }
+                if( isSafe ) {
+                    safeReports++;
+                }
+            };
+            System.out.println("safeReports: " + safeReports);
+        } catch (Exception e) { 
+            System.out.printf("Exception: " + e );
+            throw new RuntimeException(e); 
+        }
+    }
+}
---
+++ AoC20244.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.lang.Math;
+import java.lang.String;
+import java.util.HashSet;
+
+public class AoC20244 {
+    public static void main(String[] args) {
+        ArrayList<Integer> left = new ArrayList();
+        ArrayList<Integer> right = new ArrayList();
+        try {
+            String input = Files.readString(Path.of("input1.txt"));
+            for( String line : input.split("\n") ) {
+                    String[] pair = line.split("\\s+");
+                    if( pair.length == 2 ) {
+                        left.add(Integer.parseInt(pair[0]));
+                        right.add(Integer.parseInt(pair[1]));
+                    }
+            };
+
+            right.sort(null);
+            int similarityScore = left.stream().map( leftNumber -> {
+                int rightOccurrances = 0;
+                int i=right.indexOf(leftNumber);
+                if( i >= 0 ) {
+                    System.out.println( leftNumber + " found in right at " + i + "= " + right.get(i));
+                    while( right.get(i).equals(leftNumber) ) {
+                        rightOccurrances++;
+                        i++;
+                    }
+                }
+                System.out.println( leftNumber + "has " + rightOccurrances + " in right");
+                return leftNumber * rightOccurrances;
+            }).reduce(0, (acc, score) -> {
+                return acc + score;
+            });
+            System.out.println("similarityScore: " + similarityScore);
+        } catch (Exception e) { 
+            System.out.printf("Exception: " + e );
+            throw new RuntimeException(e); 
+        }
+    }
+}
