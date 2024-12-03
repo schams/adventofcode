7a6fdf2e02a40bd844b4b95598a250168c26cf2b
dschams <dan.schams@gmail.com>
2024-12-03 00:23:30 -0600

    refactor in anticipation of more involved token processing fun

--- 2024/Day3Problem2.java
+++ 2024/Day3Problem2.java
@@ -4,35 +4,49 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Day3Problem2 {
+    public static enum TokenType {
+        MUL ("mul\\((\\d{1,3}),(\\d{1,3})\\)"),
+        DONT ("don't\\(\\)"),
+        DO ("do\\(\\)");
+
+        public final String pattern;
+
+        TokenType(String pattern) {
+            this.pattern = pattern;
+        }
+    }
     public static void main(String[] args) {
         try {
             String input = Files.readString(Path.of("2024\\input4.txt"));
-            Pattern pattern = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)|do\\(\\)|don't\\(\\)");
-            System.out.println( "eh " + input);
-
-            Matcher matcher = pattern.matcher(input);
-            int total = 0;
-            boolean enabled = true;
-            while (matcher.find()) {
-                String method = matcher.group(0);
-                if (method.contains("don't")) {
-                    enabled = false;
-                } else if (method.contains("do")) {
-                    enabled = true;
-                } else if (enabled) {
-                    int a = Integer.parseInt(matcher.group(1));
-                    int b = Integer.parseInt(matcher.group(2));
-                    total += a * b;
-
-                }
-                /*int a = Integer.parseInt(matcher.group(1));
-                int b = Integer.parseInt(matcher.group(2));
-                total += a * b;*/
+            StringBuilder combinedTokenPatterns = new StringBuilder();
+            for (TokenType tokenType : TokenType.values()) {
+                combinedTokenPatterns.append(String.format("|(?<%s>%s)", tokenType.name(), tokenType.pattern));
             }
+            Pattern pattern = Pattern.compile(combinedTokenPatterns.substring(1));
+            int total = getTotal(pattern, input);
             System.out.println("total: " + total );
         } catch (Exception e) {
             System.out.printf("Exception: " + e );
             throw new RuntimeException(e);
         }
+    }
+
+    private static int getTotal(Pattern pattern, String input) {
+        Matcher matcher = pattern.matcher(input);
+        int total = 0;
+        boolean enabled = true;
+
+        while (matcher.find()) {
+            if (matcher.group(TokenType.DONT.name()) != null) {
+                enabled = false;
+            } else if (matcher.group(TokenType.DO.name()) != null) {
+                enabled = true;
+            } else if (enabled && matcher.group(TokenType.MUL.name()) != null) {
+                int a = Integer.parseInt(matcher.group(2));
+                int b = Integer.parseInt(matcher.group(3));
+                total += a * b;
+            }
+        }
+        return total;
     }
 }
